db5a6477e3e314f4ae6de099a31e65cb1f72a681
Farhadcode <farhad.ahmed.aust@gmail.com>
2022-01-11 12:57:45 +0600

    topbanner added

--- src/Component/Home/TopBanner/TopBanner.js
+++ src/Component/Home/TopBanner/TopBanner.js
@@ -1,9 +1,37 @@
-import React from 'react';
-
+import React from "react";
+import Slider from "react-slick";
 const TopBanner = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
     return (
         <div>
-            <h2> this is top banner</h2>
+
+            <Slider {...settings}>
+                <div>
+                    <img src={`https://ibb.co/mHxSrqm`} alt="" />
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+                <div>
+                    <h3>4</h3>
+                </div>
+                <div>
+                    <h3>5</h3>
+                </div>
+                <div>
+                    <h3>6</h3>
+                </div>
+            </Slider>
         </div>
     );
 };
